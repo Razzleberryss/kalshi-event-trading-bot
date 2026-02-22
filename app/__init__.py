7e84654ad05b23a9bbddeab0cc096487d5a88294
Razzleberryss <razzleberryss21@icloud.com>
2026-02-22 02:15:49 -0600

    fix: remove app.main import from app/__init__ to fix circular dependency in tests

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
 """app - EventTradingBot application package."""
-from app.main import EventTradingBot
 from app.market_scanner import MarketScanner
 from app.strategy import evaluate_market, score_market, is_tradeable, SCORE_THRESHOLD
 
 __all__ = [
-    "EventTradingBot",
     "MarketScanner",
     "evaluate_market",
     "score_market",
