82ec095237fc93bb6b7734581ed2500ec2fe309a
Razzleberryss <razzleberryss21@icloud.com>
2026-02-23 01:51:06 -0600

    fix: remove KalshiAPIError from clients __init__ import

--- clients/__init__.py
+++ clients/__init__.py
@@ -1,4 +1,4 @@
-"""clients - Kalshi API client package."""
-from clients.kalshi_client import AsyncKalshiClient, KalshiAPIError
+"""clients package."""
+from clients.kalshi_client import AsyncKalshiClient
 
-__all__ = ["AsyncKalshiClient", "KalshiAPIError"]
+__all__ = ["AsyncKalshiClient"]
